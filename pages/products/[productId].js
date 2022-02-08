30d9406997139f6c7e41021b605e8397a033aa89
Charmaine Garcia <garciacharmaine95@gmail.com>
2022-02-08 20:49:33 +0100

    Add price and input field for quantity to single product page

--- pages/products/[productId].js
+++ pages/products/[productId].js
@@ -11,12 +11,20 @@ export default function SingleProduct(props) {
         <meta description="all yarns" />
       </Head>
       <h1>Yarn {props.yarn.name}</h1>
-      <Image src={`/allyarns/${props.yarn.id}.jpg`} width="300" height="300" />
+      <Image
+        data-test-id="product-image"
+        alt="yarn image"
+        src={`/allyarns/${props.yarn.id}.jpg`}
+        width="300"
+        height="300"
+      />
 
       <div>id: {props.yarn.id}</div>
       <div>name: {props.yarn.name}</div>
       <div>type: {props.yarn.type}</div>
       <div>color: {props.yarn.color}</div>
+      <div data-test-id="product-price">price: {props.yarn.price}</div>
+      <input type="number" id="quantity" name="quantity" min="1" />
     </Layout>
   );
 }
--- pages/util/database.js
+++ pages/util/database.js
@@ -1,10 +1,28 @@
 const yarnsDatabase = [
-  { id: '1', name: 'Cool Wool', type: 'Merino', color: 'Ecru' },
-  { id: '2', name: 'Landlust Alpaca', type: 'Alpaca', color: 'Lightgrey' },
-  { id: '3', name: 'Soft Cotton', type: 'Cotton', color: 'Beige' },
-  { id: '4', name: 'Mohair Trend', type: 'Mohair', color: 'Turquoise' },
-  { id: '5', name: 'Silkhair', type: 'Silk', color: 'Pastelgreen' },
-  { id: '6', name: 'Cool Wool Cashmere', type: 'Cashmere', color: 'Pink' },
+  { id: '1', name: 'Cool Wool', type: 'Merino', color: 'Ecru', price: 6 },
+  {
+    id: '2',
+    name: 'Landlust Alpaca',
+    type: 'Alpaca',
+    color: 'Lightgrey',
+    price: 8,
+  },
+  { id: '3', name: 'Soft Cotton', type: 'Cotton', color: 'Beige', price: 5 },
+  {
+    id: '4',
+    name: 'Mohair Trend',
+    type: 'Mohair',
+    color: 'Turquoise',
+    price: 10,
+  },
+  { id: '5', name: 'Silkhair', type: 'Silk', color: 'Pastelgreen', price: 15 },
+  {
+    id: '6',
+    name: 'Cool Wool Cashmere',
+    type: 'Cashmere',
+    color: 'Pink',
+    price: 18,
+  },
 ];
 
 export default yarnsDatabase;
