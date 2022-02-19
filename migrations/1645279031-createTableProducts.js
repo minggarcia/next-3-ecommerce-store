9aa167d9f328183fc8e928f7695e0845e81e6ddb
Charmaine Garcia <garciacharmaine95@gmail.com>
2022-02-19 16:00:14 +0100

    Edit migrations

--- migrations/1645279031-createTableProducts.js
+++ migrations/1645279031-createTableProducts.js
@@ -1,7 +1,17 @@
-exports.up = async client => {
-	// <insert magic here>
+exports.up = async (sql) => {
+  console.log('creating table products ...');
+  await sql`
+	CREATE TABLE products (
+		id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+		name varchar(50) NOT NULL,
+		type varchar(50) NOT NULL,
+		color varchar(20) NOT NULL,
+		price integer NOT NULL
+	);`;
 };
 
-exports.down = async client => {
-	// just in case...
+exports.down = async (sql) => {
+  console.log('dropping table products...');
+  await sql`
+	DROP TABLE products`;
 };
